5aca8ac31ea39d440f16d9af9f25bd0a641f57bc
steffijoji <steffijoji@gmail.com>
2023-08-30 22:33:56 -0300

    refactor: add use of page objects

--- cypress/e2e/desafio.cy.js
+++ cypress/e2e/desafio.cy.js
@@ -14,86 +14,63 @@ describe('Cadastro de usuário', () => {
     })
 
     it('Validar campo nome vazio', () => {
-        cy.get('#user')
-            .should('not.have.text')
 
+        cy.get('#user').should('not.have.text')
         cy.saveRegister()
-
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo nome deve ser prenchido')
+        cy.checkMessage('O campo nome deve ser prenchido')
     })
 
-    it('Validar campo e-mail vazio', () => {       
-        
-        //usando faker para gera nome
+    it('Validar campo e-mail vazio', () => {
+
+        //usando faker para gerar nome
         cy.fillName(faker.person.fullName)
-
-        cy.get('#email')
-           .should('not.have.text')
-
+        cy.get('#email').should('not.have.text')
         cy.saveRegister()
-
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
+        cy.checkMessage('O campo e-mail deve ser prenchido corretamente')
     })
 
-    it('Validar campo e-mail inválido', () => {           
+    it('Validar campo e-mail inválido', () => {
 
         //usando constante declarada em fixtures
         cy.fillName(valid_data.name)
-
         //digitando o valor diretamente no parâmetro
-        cy.fillEmail('email_invalido')
-            
+        cy.fillEmail('email_invalido') 
         cy.saveRegister()
-
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo e-mail deve ser prenchido corretamente')
+        cy.checkMessage('O campo e-mail deve ser prenchido corretamente')
     })
 
-    it.only('Validar campo senha vazia', () => {          
+    it('Validar campo senha vazia', () => {
 
         cy.fillName(valid_data.name)
         cy.fillEmail(valid_data.email)
-
-        cy.get('#password')
-            .should('not.have.text')
-            
+        cy.get('#password').should('not.have.text')
         cy.saveRegister()
-
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
+        cy.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
     })
 
-    it('Validar campo senha inválida', () => {      
+    it('Validar campo senha inválida', () => {
 
         cy.fillName(valid_data.name)
         cy.fillEmail(valid_data.email)
         cy.fillPassword('123')
         cy.saveRegister()
-
-        cy.get('#errorMessageFirstName')
-            .should('be.visible')
-            .should('have.text', 'O campo senha deve ter pelo menos 6 dígitos')
+        cy.checkMessage('O campo senha deve ter pelo menos 6 dígitos')
     })
 
-    it('Validar cadastro realizado com sucesso', () => {
+    it.only('Validar cadastro realizado com sucesso', () => {
 
         cy.fillName(valid_data.name)
         cy.fillEmail(valid_data.email)
         cy.fillPassword(valid_data.password)
         cy.saveRegister()
+        cy.checkRegisterSuccess(valid_data.name)
 
-        cy.get('#swal2-title')
+/*      cy.get('#swal2-title')
             .should('be.visible')
             .should('have.text', 'Cadastro realizado!')
         
         cy.get('#swal2-html-container')
         .should('be.visible')
-        .should('contain', `Bem-vindo ${valid_data.name}`)
+        .should('contain', `Bem-vindo ${valid_data.name}`) */
     })
 })
--- cypress/support/register_page_commands.js
+++ cypress/support/register_page_commands.js
@@ -2,20 +2,40 @@
 
 Cypress.Commands.add('saveRegister', () => {
     cy.get('#btnRegister')
+        .should('be.visible')
         .click()
 })
 
 Cypress.Commands.add('fillName', (name) => {
     cy.get('#user')
+        .should('be.visible')
         .type(name)
 })
 
 Cypress.Commands.add('fillEmail', (email) => {
     cy.get('#email')
+        .should('be.visible')
         .type(email)
 })
 
 Cypress.Commands.add('fillPassword', (password) => {
     cy.get('#password')
-            .type(password)
+        .should('be.visible')
+        .type(password)
+})
+
+Cypress.Commands.add('checkMessage', (message) => {
+    cy.get('#errorMessageFirstName')
+        .should('be.visible')
+        .should('have.text', message)
+})
+
+Cypress.Commands.add('checkRegisterSuccess', (name) => {
+    cy.get('#swal2-title') 
+        .should('be.visible')
+        .should('have.text', 'Cadastro realizado!')
+
+    cy.get('#swal2-html-container', {timeout: 3000})
+        .should('be.visible')
+        .should('contain', `Bem-vindo ${name}`)
 })
