1272a869adef274b0f16a14defbd42e3b568d0a9
steffijoji <steffijoji@gmail.com>
2023-08-30 23:00:41 -0300

    refactor: separate elements and methods

--- cypress/support/register_page_commands.js
+++ cypress/support/register_page_commands.js
@@ -1,41 +1,61 @@
 /// <reference types="cypress" />
 
+
+// Elementos
+const elements = {
+    buttons: {
+        register: '#btnRegister'
+    },
+    fields: {
+        name: '#user',
+        email: '#email',
+        password: '#password'
+    },
+    messages: {
+        error: '#errorMessageFirstName',
+        successTitle: '#swal2-title',
+        successSubtitle: '#swal2-html-container'
+    }
+}
+
+
+// Ações/Métodos/Funções
 Cypress.Commands.add('saveRegister', () => {
-    cy.get('#btnRegister')
+    cy.get(elements.buttons.register)
         .should('be.visible')
         .click()
 })
 
 Cypress.Commands.add('fillName', (name) => {
-    cy.get('#user')
+    cy.get(elements.fields.name)
         .should('be.visible')
         .type(name)
 })
 
 Cypress.Commands.add('fillEmail', (email) => {
-    cy.get('#email')
+    cy.get(elements.fields.email)
         .should('be.visible')
         .type(email)
 })
 
 Cypress.Commands.add('fillPassword', (password) => {
-    cy.get('#password')
+    cy.get(elements.fields.password)
         .should('be.visible')
         .type(password)
 })
 
 Cypress.Commands.add('checkMessage', (message) => {
-    cy.get('#errorMessageFirstName')
+    cy.get(elements.messages.error)
         .should('be.visible')
         .should('have.text', message)
 })
 
 Cypress.Commands.add('checkRegisterSuccess', (name) => {
-    cy.get('#swal2-title') 
+    cy.get(elements.messages.successTitle) 
         .should('be.visible')
         .should('have.text', 'Cadastro realizado!')
 
-    cy.get('#swal2-html-container', {timeout: 3000})
+    cy.get(elements.messages.successSubtitle, {timeout: 3000})
         .should('be.visible')
         .should('contain', `Bem-vindo ${name}`)
 })
